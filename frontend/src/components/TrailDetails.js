726ef1dacd1c71d600523a5689fd1ef0352f2b58
keaton <keatonmcarlton@gmail.com>
2024-12-05 00:41:57 -0500

    added newline for readability. more coming soon

--- frontend/src/components/TrailDetails.js
+++ frontend/src/components/TrailDetails.js
@@ -6,8 +6,9 @@ const TrailDetails = ({trail}) => {
         <div className = "TrailDetails">
             <h4>{<DisplayDate dateString={trail.date} />}</h4>
             <p><strong>DURATION:</strong>{trail.duration}</p>
-            <p><strong>TIME:</strong>{trail.quality}</p>
+            <p><strong>QUALITY:</strong>{trail.quality}</p>
             <p>{trail.createdAt}</p>
+            <br />
         </div>
     )
 }
