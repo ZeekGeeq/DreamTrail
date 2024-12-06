f789e880fa838d35dc2f934b797b984d3102e97e
keaton <keatonmcarlton@gmail.com>
2024-12-06 16:26:50 -0500

    hides trail and stats page when not authenticated. removed calendar api request.

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -9,12 +9,14 @@ const Navbar = ({ isAuthenticated }) => {
       <div class = 'sidebar'>
         <ul>
         {isAuthenticated ? (
+          <>
             <li><Link to="/profile">Profile</Link></li>
+            <li><Link to='/trails'>Trails</Link></li>
+            <li><Link to='/stats'>Stats</Link></li>
+          </>
           ) : (
             <li><Link to="/login">Login</Link></li>
           )}
-            <li><Link to='/trails'>Trails</Link></li>
-            <li><Link to='/stats'>Stats</Link></li>
         </ul>
       </div>
     </section>
